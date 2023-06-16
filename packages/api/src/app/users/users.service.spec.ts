75fbe94f672e6a4e9ae43aec10536a71f24df24c
Romain Bellande <rbellande@wedigital.garden>
2023-06-16 14:20:22 +0200

    feat: unit tests
    refs: #1

--- packages/api/src/app/users/users.service.spec.ts
+++ packages/api/src/app/users/users.service.spec.ts
@@ -1,12 +1,36 @@
+import { createUserInputFixture, userFixture } from './user.fixture';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { EntityManager } from '@mikro-orm/postgresql';
+import { getRepositoryToken } from '@mikro-orm/nestjs';
+import { User } from './entities/user.entity';
 
 describe('UsersService', () => {
   let service: UsersService;
 
+  const mockEntityManager = {
+    persistAndFlush: jest.fn().mockImplementation(() => {
+      return Promise.resolve();
+    }),
+  };
+
+  const mockUserRepository = {
+    create: jest.fn().mockImplementation(() => Promise.resolve(userFixture)),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository,
+        },
+        {
+          provide: EntityManager,
+          useValue: mockEntityManager,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -14,5 +38,9 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a new user', async () => {
+    expect(await service.create(createUserInputFixture)).toEqual(userFixture);
   });
 });
