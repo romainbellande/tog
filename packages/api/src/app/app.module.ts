25b579566a83d53bcb29bc578098c5cd4694a52d
Naimor <romain.bellande@orange.fr>
2023-07-09 09:18:06 +0200

    chore(api): seeder run command (#20)
    closes: #20

--- packages/api/src/app/app.module.ts
+++ packages/api/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import {
+  Logger,
   MiddlewareConsumer,
   Module,
   NestModule,
@@ -10,14 +11,20 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { pinoLoggerFactory } from '@api/utils/pino-logger-factory';
-import { MikroORM } from '@mikro-orm/core';
+import { MikroORM, ReflectMetadataProvider } from '@mikro-orm/core';
 import { LoggerModule } from 'nestjs-pino';
+import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 
 import { UsersModule } from './users/users.module';
 import { CommandsModule } from './commands/commands.module';
-import MikroOrmConfig from '../mikro-orm.config';
-import { validationSchema, configuration } from '../configuration';
+import {
+  validationSchema,
+  configuration,
+  Configuration,
+} from '../configuration';
+import { migrationsList } from '@api/utils';
 
+const MikroOrmLogger = new Logger('MikroORM');
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -30,7 +37,25 @@ import { validationSchema, configuration } from '../configuration';
       useFactory: pinoLoggerFactory,
       inject: [ConfigService],
     }),
-    MikroOrmModule.forRoot(MikroOrmConfig),
+    MikroOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService<Configuration>) => ({
+        autoLoadEntities: true,
+        clientUrl: configService.get('databaseUrl'),
+        metadataProvider: ReflectMetadataProvider,
+        driver: PostgreSqlDriver,
+        discovery: { disableDynamicFileAccess: true },
+        registerRequestContext: false,
+        type: 'postgresql',
+        migrations: {
+          migrationsList,
+        },
+        validate: false,
+        validateRequired: false,
+        logger: MikroOrmLogger.log.bind(MikroOrmLogger),
+      }),
+      inject: [ConfigService],
+    }),
     CommandsModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
